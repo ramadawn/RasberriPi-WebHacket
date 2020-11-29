f628f80d8a1fbf1fe65f3f7c5bf04ecc8341f47d
Douglas Oak <ramadawn@gmail.com>
2020-11-28 20:25:02 -0800

    Code : AdvancedIPScanner
     modified the port scanner function so that it now returns a list of IPs with a particular open port, a list of Ips on the network with active machines and a dictionary with a key for each active IP and the manufacturer name as the value.
    
    Call it as such
    
    Open Ports, IPList, Manufacturer_Dictionary 
    = findOpenPorts(port)

---
+++ ImprovedScanner.py
@@ -0,0 +1,89 @@
+import nmap3
+import json
+import pprint
+import socket
+from LocalIP import returnIPNet
+
+
+#will look for open ports on a network and return list of IPs with that open port
+def ImprovedScanner(port, scanRange = 255,network = "no input"): #network must be an iP address that ends in .0 ex 192.168.1.0
+
+    maxIP = scanRange
+    Avail_IP_List = []
+
+    vendorDict = {}
+
+    #Get IP network of local machine
+    IP_network = returnIPNet()
+
+    #if not ip address was added us the local IP
+    if(network != "no input"):
+        IP_network = network
+
+    #Grab Prefex
+    IP_prefex = IP_network[:-1]
+
+    #Start nmap connection
+    nmap = nmap3.NmapScanTechniques()
+
+
+
+
+    #ping all ips in network and generate list of machines 
+    for location in range(1,maxIP):
+        IP_Address = IP_prefex + str(location)
+        results = nmap.nmap_ping_scan(IP_Address)
+        print("Pinging " + IP_Address + " ...", end='')
+        
+        try:
+            data = results[0]
+            print()
+            Avail_IP_List.append(IP_Address)
+            vendorDict[IP_Address] = ""
+            print("Machine found at " + IP_Address + "...",end='')
+        except:
+            print(" no machine ",end='')
+
+
+        try:
+            data = results[0]
+            vendorInfo = data['addresses'][1]['vendor']
+            vendorDict[IP_Address] = vendorInfo
+            print(" made by " + vendorInfo)
+        except:
+            print(" no vendor")
+            
+
+    
+    #port scanning phase
+
+    ports = nmap3.Nmap()
+    print("Port " + str(port) + " scan")
+    a_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    openPortList = []
+
+
+    #scan for open port useing list of active machines
+    for IPs in Avail_IP_List:
+        location = (IPs, port)
+        result_of_check = a_socket.connect_ex(location)
+
+        print(result_of_check)
+
+        if result_of_check == 0:
+           print("Port " + str(port) + " on " + IPs + " is open adding to list")
+           openPortList.append(IPs)
+        else:
+           print("Port on " + IPs + " is not open")
+        
+
+    a_socket.close()
+
+    return openPortList, Avail_IP_List, vendorDict
+
+
+
+
+
+
+
