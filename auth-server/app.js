7a58757098051bfe40be802869508afc96b930fe
Monther Balghonaim <mab_only@windowslive.com>
2020-11-29 22:23:43 -0800

    Fix auth server bug

--- auth-server/app.js
+++ auth-server/app.js
@@ -34,7 +34,7 @@ app.post('/auth', function (request, response) {
 });
 
 app.get('/home', function (request, response) {
-    response.send('Welcome back, ' + request.session.username + '!');
+    response.send('Welcome back, ' + USERNAME + '!');
     response.end();
 });
 
